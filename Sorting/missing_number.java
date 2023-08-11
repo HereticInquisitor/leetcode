77e38c23d4f751d555cd2f48fb7c092f3fae53a4
Heretic Inquisitor <ayushraj7353@gmail.com>
2023-08-12 01:46:14 +0530

    e

--- Sorting/missing_number.java
+++ Sorting/missing_number.java
@@ -2,21 +2,29 @@ import java.util.*;
 public class missing_number {
     public static void main(String args[]) {
         int [] arr= {3,0,1};
-        sort(arr);
-        System.out.println(Arrays.toString(arr));
+        int f=missing(arr);
+        System.out.println(f);
     }
 
-    static void sort(int [] arr) {
+    static int missing(int [] arr) {
         int i=0;
         while(i<arr.length) {
             int correct =arr[i]-1;
-            if(arr[i] != arr[correct]) {
+            if(arr[i] < arr.length && arr[i] != arr[correct]) {
                 swap(arr, i, correct);
             }
             else {
                 i++;
             }
         }
+        // Search for the first missing number
+        for(int index=0; index< arr.length; index++) {
+            if(arr[index] != index) {
+                return index;
+            }
+
+        }
+        return arr.length;
     }
 
     static void swap(int [] arr , int first, int second) {
